46c154ae667df50af2d49a904c8b4727086b0996
sergey <aserhii@protonmail.com>
2018-06-29 20:52:47 +0300

    Initialize the Database

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,6 +11,9 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
+    from . import db
+    db.init_app(app)
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
@@ -27,6 +30,6 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'Hello, World!'
+        return 'Hello, World! How are you?'
 
     return app
